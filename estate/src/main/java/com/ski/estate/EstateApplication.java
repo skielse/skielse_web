7f2a67e32b73a381b767e56bd82a87c120af0c7f
skielse <marine0228@gmail.com>
2024-11-07 20:34:45 +0800

    add estate module

---
+++ estate/src/main/java/com/ski/estate/EstateApplication.java
@@ -0,0 +1,16 @@
+package com.ski.estate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ski
+ */
+@SpringBootApplication
+public class EstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateApplication.class, args);
+	}
+
+}
