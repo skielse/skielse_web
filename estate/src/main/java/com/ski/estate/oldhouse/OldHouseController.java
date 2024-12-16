9955bca4ef13d77939fd078eecae072034bcae2d
skielse <marine0228@gmail.com>
2024-12-16 16:21:16 +0800

    add HouseProcessor

--- estate/src/main/java/com/ski/estate/oldhouse/OldHouseController.java
+++ estate/src/main/java/com/ski/estate/oldhouse/OldHouseController.java
@@ -1,8 +1,11 @@
 package com.ski.estate.oldhouse;
 
+import com.ski.estate.oldhouse.bean.House;
 import com.ski.estate.oldhouse.bean.Region;
+import com.ski.estate.oldhouse.processor.HouseProcessor;
 import com.ski.estate.oldhouse.processor.RegionProcessor;
 import com.ski.estate.utils.IDGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import us.codecraft.webmagic.Spider;
@@ -13,24 +16,28 @@ import java.util.List;
  * @author skielse
  * @date 2024/12/10
  */
+@Slf4j
 @RestController
 @RequestMapping("old-house")
 public class OldHouseController {
 
     @RequestMapping("scan")
-    public String getOldHouse(){
+    public String getOldHouse() {
         String oldHouseHomeUrl = "https://zz.lianjia.com/";
         RegionProcessor regionProcessor = new RegionProcessor();
         Spider.create(regionProcessor).addUrl(oldHouseHomeUrl + "ershoufang/").thread(1).run();
         List<Region> regionList = regionProcessor.getRegionList();
         long taskId = IDGenerator.getTaskId();
         for (Region region : regionList) {
-            String regioonHouoseUrl = oldHouseHomeUrl + region.getRegionUrl() + "pg1";
-            System.out.println(regioonHouoseUrl);
+            String regionHourlies = oldHouseHomeUrl + region.getRegionUrl() + "pg1";
+            log.info("regionHourlies = {}", regionHourlies);
             HouseProcessor houseProcessor = new HouseProcessor("郑州", region.getRegionName());
-            Spider.create(houseProcessor).addUrl(regioonHouoseUrl).thread(3).run();
+            Spider.create(houseProcessor).addUrl(regionHourlies).thread(3).run();
             List<House> houseList = houseProcessor.getHouseList();
-            new HouseDao().inseertHoue(houseList, "郑州", region.getRegionName(), taskId);
+            for (House house : houseList) {
+                log.info(house.toString());
+            }
+//            new HouseDao().inseertHoue(houseList, "郑州", region.getRegionName(), taskId);
         }
         return "old house";
     }
